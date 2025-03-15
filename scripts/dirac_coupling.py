c106689a27a9667f1490ec6dbd60ca4276964336
alifayyazmalik <alifayyaz@live.com>
2025-03-16 04:05:25 +0500

    Create dirac_coupling.py

---
+++ scripts/dirac_coupling.py
@@ -0,0 +1,9 @@
+# Generate Dirac-Tpm coupling schematic (Figure 1)
+import matplotlib.pyplot as plt
+
+def plot_dirac_tpm_coupling():
+    # Toy visualization code here
+    plt.savefig('figures/Dirac_Tpm_schematic.png')
+
+if __name__ == "__main__":
+    plot_dirac_tpm_coupling()
